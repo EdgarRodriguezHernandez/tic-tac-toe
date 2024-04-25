4e418373ee0181526417866f42c10536fbaca10d
EdgarRodríguezHernán <edgarL2D18@gmail.com>
2024-04-25 18:56:37 +0200

    Last changes, winner and turn

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@
 import { useState } from 'react';
 import './App.css'
 
-const TURNS = {//Creación de turnos
-  X: 'X',//elemento si es X o O
+const TURNS = {
+  X: 'X',
   O: 'O'
 }
 
 
-//tablero. Children la propiedad de dentro(X, O), Update para actualizar y el index
+//Tablero
 const Square = ({children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'selected' : ''}`
+  const className = `square ${isSelected ? 'is-selected' : ''}`
  
   const handleClick = () => {
   updateBoard(index)
   }
 
-  //Renderizamos el square
   return (
     <div onClick={handleClick} className={className}>
     {children}
@@ -24,30 +23,63 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const Winner_Combos = [
+/*We could simplify the code more, creating abc position conditionals*/
+//horizontal
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+//vertical
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+//cross
+[0, 4, 8],
+[2, 4, 6]
+]
+
 
 
 function App() {
 
     //creación de tablero posición inicial y actual
-    const [board, setBoard] = useState/*Estado inicial*/(Array(9).fill(null));
-    //En css con grid le distribuimos las 9 posiciones con grid en index.css
+    const [board, setBoard] = useState(Array(9).fill(null));
 
-    //estado de comienzo de turno. Array de 2pos , estado y como actualizar
+
+    //estado de comienzo de turno.
     const [turn, setTurn] = useState(TURNS.X)
 
-    const updateBoard = (index) => {
-      //Si en el board hay alguna cosa, que no se acrtualize
-      if(board[index]) return
+    const[winner, setWinner] = useState(null)
 
+    const checkWinner = (boardCheck) => {
+      for (const combo of Winner_Combos){
+        const [a, b, c] = combo
+        if(
+          boardCheck[a] && boardCheck[a] === boardCheck[b] && 
+          boardCheck[a] === boardCheck[c]
+        ){
+          return boardCheck[a]
+        }
+        return (null)
+      }
+    }
+
+    const updateBoard = (index) => {
+      if(board[index] || winner) return
 
       //Actualiza el tablero
-      const newBoard = [...board]//copia el array aquí
-      newBoard[index]= turn //Turno X o O
+      const newBoard = [...board]
+      newBoard[index]= turn 
       setBoard(newBoard)
 
       //cambiar turno
       const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-      setTurn(newTurn)//Actualizamos el turno con setTurn
+      setTurn(newTurn)
+
+      const newWinner = checkWinner (newBoard)
+      if(newWinner){
+        setWinner(newWinner)
+      }
     }
 
   return(
@@ -56,12 +88,12 @@ function App() {
       
       <section className = "game">
         {
-          board.map((cell, index) => {//lista de elemetnos en array
+          board.map((_, index) => {
             return (
               <Square
-              key = {index} /*El indice en este caso no varia*/
+              key = {index} 
               index ={index}
-              updateBoard={updateBoard}//Funcion para actualizar estados....
+              updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
@@ -70,6 +102,7 @@ function App() {
         }
 
       </section>
+
       <section className="turn">
         <Square isSelected ={turn === TURNS.X}>
           {TURNS.X}
